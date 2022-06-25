bf21eaf0df35a5aedad1b8e20bbde02e59d51e96
aadarsh012 <aadarsh012@gmail.com>
2022-06-26 02:21:40 +0530

    Added Alan AI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const alanApiKey = "3f4a6bc5a5076c4ca7347dbe45bb3d392e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    alanBtn({
+      key: alanApiKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          console.log(articles);
+          setNews(articles);
+        }
+      }
+    });
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;
