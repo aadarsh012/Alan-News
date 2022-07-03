78993d762e4e46e0a4e4cf0f024b3c5a220d45a5
aadarsh012 <aadarsh012@gmail.com>
2022-07-03 10:42:45 +0530

    Added Timer

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 
 const Navbar = () => {
+  const [hours, setHours] = useState();
+  const [minutes, setMinutes] = useState();
+  const [seconds, setSeconds] = useState();
+
+  const updateTime = () => {
+    const date = new Date();
+    setHours(date.getHours());
+    setMinutes(date.getMinutes());
+    setSeconds(date.getSeconds());
+  };
+
+  setInterval(updateTime, 1000);
+
   return (
     <>
       <AppBar position="sticky" sx={{ bgcolor: "transparent", backdropFilter: "blur(5px)" }}>
         <Toolbar>
           <Typography variant="h5" sx={{ flexGrow: 1 }}>
             LOGO
+          </Typography>
+          <Typography variant="h6">
+            {hours < 10 ? "0" + hours : hours} : {minutes < 10 ? "0" + minutes : minutes} :{" "}
+            {seconds < 10 ? "0" + seconds : seconds}
           </Typography>
         </Toolbar>
       </AppBar>
